7b74c78aaef1a00c4a97d35e387cfaa016cd07dc
inkfinity1 <derek.wu@kingsway.school.nz>
2025-09-08 15:03:21 +1200

    Version 1 Derek Wu and Joshua Lee
    Version 1 Derek Wu and Joshua Lee

---
+++ Version 1 Derek Wu and Joshau Lee/code/main.py
@@ -0,0 +1,178 @@
+# Ngā Taonga o Māui (The Treasures of Māui)
+# GAME VERSION 1.7
+# AUTHOR: Derek WU
+# Date: 2025/09/01
+# Set up
+import pygame
+import sys
+from main_asset import load_assets
+from sound_asset import load_sounds
+from settings_for_levels import Level
+from map import level_map_1, level_map_2, level_map_3, level_map_4
+from maui_hunting import PlatformerGame
+
+# Init
+pygame.init()
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Ngā Taonga o Māui")
+clock = pygame.time.Clock()
+
+# Load assets
+(background_img, information_button_img, info_img, game_button, background_level_one,
+ background_level_two, background_level_three, background_level_four, instruction_button, home_button, exit_button, 
+ instruction_page, objective) = load_assets()
+
+click_sound = load_sounds()
+pygame.mixer.music.set_volume(0.2)
+#pygame.mixer.music.play(-1)  # Loop forever
+#pygame.mixer.music.play(999) # play the music 999 times
+pygame.mixer.music.play(-1, fade_ms= 1000)  # Loop foverevr and 1 second fade-in each time
+
+# Rects
+information_button_rect = information_button_img.get_rect(topleft=(0, 0))
+game_button_rect = game_button.get_rect(topleft=(250, 200))
+instruction_button_rect = instruction_button.get_rect(topleft=(250, 500))
+home_button_rect = home_button.get_rect(topleft=(10, HEIGHT - home_button.get_height() - 10))
+exit_button_rect = exit_button.get_rect(bottomright=(WIDTH - 10, HEIGHT - 10))
+
+# Game state
+in_main_menu = True
+current_level_index = 0
+levels = [level_map_1, level_map_2, level_map_3, level_map_4]
+backgrounds = [background_level_one, background_level_two, background_level_three,background_level_four]
+level = Level(levels[current_level_index], screen)
+
+# Main loop that run the game
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Main menu interactions
+        elif in_main_menu and event.type == pygame.MOUSEBUTTONDOWN: # Information page if the button is press.
+            if information_button_rect.collidepoint(event.pos):
+                if click_sound:     # Play a sound when the button is press.
+                    click_sound.play()
+
+                screen.blit(info_img, (0, 0)) # Screen locaton and open's the imformation page
+                pygame.display.update()
+
+                info_screen = True 
+                while info_screen:  # Any key or mouse is press exit the page.
+                    for info_event in pygame.event.get():
+                        if info_event.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit()
+                        elif info_event.type in (pygame.KEYDOWN, pygame.MOUSEBUTTONDOWN): # Any key or mouse is press exit the page.
+                            info_screen = False
+                            if click_sound: # play a sound
+                                click_sound.play()
+
+            elif instruction_button_rect.collidepoint(event.pos): # Instruction page
+                if click_sound: # # Play a sound when the key or mouse is press.
+                    click_sound.play()
+
+                screen.blit(instruction_page , (0, 0)) # Screen location and open's the instruction page
+                pygame.display.update()
+
+                instruction_screen = True 
+                while instruction_screen:
+                    for instruction_event in pygame.event.get():
+                        if instruction_event.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit() 
+                        elif instruction_event.type in (pygame.KEYDOWN, pygame.MOUSEBUTTONDOWN): # Any key or mouse is press exit the page.
+                            instruction_screen = False
+                            if click_sound: # play sound when any key or mouse is press.
+                                click_sound.play() 
+
+            elif game_button_rect.collidepoint(event.pos):  # Game button press
+                if click_sound:
+                    click_sound.play() # Play sound when game button is press
+
+                # Show objective page
+                screen.blit(objective, (0, 0)) # Screen locaton and open's the objective page
+                pygame.display.update()
+
+                objective_screen = True
+                while objective_screen:
+                    for obj_event in pygame.event.get():
+                        if obj_event.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit() 
+                        elif obj_event.type in (pygame.KEYDOWN, pygame.MOUSEBUTTONDOWN): # Any key or mouse is press exit the page.
+                            objective_screen = False
+                            if click_sound: # play sound when any key or mouse is press.
+                                click_sound.play()
+
+                # Show instruction page
+                screen.blit(instruction_page, (0, 0)) # Screen locaton and open's the instruction page
+                pygame.display.update()
+
+                instruction_screen = True
+                while instruction_screen:
+                    for instruction_event in pygame.event.get():
+                        if instruction_event.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit()
+                        elif instruction_event.type in (pygame.KEYDOWN, pygame.MOUSEBUTTONDOWN): # Any key or mouse is press exit the page.
+                            instruction_screen = False
+                            if click_sound: # play sound when any key or mouse is press.
+                                click_sound.play()
+
+                # Start the game
+                in_main_menu = False
+                
+                # Side buttons
+            elif exit_button_rect.collidepoint(event.pos):  # Exit from main menu
+                if click_sound:
+                    click_sound.play()
+                    pygame.time.delay(300) # It give time for the sound to play becasue it exit the gaame terminal
+                running = False
+        # In game interactions
+        elif not in_main_menu and event.type == pygame.MOUSEBUTTONDOWN: # Home/Menu page and exit the game.
+            if home_button_rect.collidepoint(event.pos):
+                if click_sound:
+                    click_sound.play() # Play sound when mouse is press the button.
+
+                in_main_menu = True  # Return to main menu
+            elif exit_button_rect.collidepoint(event.pos):  # Exit from game screen
+                if click_sound:
+                    click_sound.play()  # Play sound when mouse is press the button.
+                    pygame.time.delay(300) # It give time for the click sound to play when the user exit the game terminal
+                running = False
+    # Rendering/ load the grahics.
+    if in_main_menu:
+        screen.blit(background_img, (0, 0))
+        screen.blit(information_button_img, information_button_rect)
+        screen.blit(game_button, game_button_rect)
+        screen.blit(instruction_button, instruction_button_rect)
+        screen.blit(exit_button, exit_button_rect) 
+    else:
+        screen.blit(backgrounds[current_level_index], (0, 0))
+        screen.blit(home_button, home_button_rect) 
+        screen.blit(exit_button, exit_button_rect)  
+
+        if level.run():  # If player reaches transition tile
+            current_level_index += 1
+            if current_level_index < len(levels):
+                level = Level(levels[current_level_index], screen)
+            else:
+                # Launch PlatformerGame
+                game_instance = PlatformerGame(screen)
+                game_instance.run()
+                
+                print("All levels completed well done!")
+                in_main_menu = True
+                current_level_index = 0
+                level = Level(levels[current_level_index], screen)
+
+    pygame.display.update()
+    clock.tick(60)
+
+# Exit
+pygame.quit()
+sys.exit()
+
